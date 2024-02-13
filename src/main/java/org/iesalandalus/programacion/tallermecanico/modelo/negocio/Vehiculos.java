e920c70510152f11e1f00b96076f1a734733904b
MacREpiC <brosgenepic@gmail.com>
2024-02-13 10:43:37 +0100

    Terminada la clase Vehiculos

---
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/negocio/Vehiculos.java
@@ -0,0 +1,42 @@
+package org.iesalandalus.programacion.tallermecanico.modelo.negocio;
+import org.iesalandalus.programacion.tallermecanico.modelo.dominio.Vehiculo;
+
+import javax.naming.OperationNotSupportedException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Vehiculos {
+    private final List<Vehiculo> listaVehiculos;
+
+    public Vehiculos() {
+        listaVehiculos = new ArrayList<>();
+    }
+
+    public List<Vehiculo> get() {
+        return new ArrayList<>(listaVehiculos);
+    }
+
+    public Vehiculo buscar(Vehiculo vehiculo){
+        Objects.requireNonNull(vehiculo, "No se puede buscar un vehículo nulo.");
+        int indice = listaVehiculos.indexOf(vehiculo);
+        return (indice == -1) ? null : listaVehiculos.get(indice);
+    }
+
+    public void insertar(Vehiculo vehiculo) throws OperationNotSupportedException {
+        Objects.requireNonNull(vehiculo, "No se puede insertar un vehículo nulo.");
+        if(buscar(vehiculo) != null){
+            throw new OperationNotSupportedException("Ya existe un vehículo con esa matrícula.");
+        }
+        listaVehiculos.add(vehiculo);
+    }
+
+    public void borrar(Vehiculo vehiculo) throws OperationNotSupportedException {
+        Objects.requireNonNull(vehiculo, "No se puede borrar un vehiculo nulo.");
+        if(buscar(vehiculo) == null){
+            throw new OperationNotSupportedException("No existe ningún vehiculo con esa matrícula.");
+
+        }
+        listaVehiculos.remove(vehiculo);
+    }
+}
