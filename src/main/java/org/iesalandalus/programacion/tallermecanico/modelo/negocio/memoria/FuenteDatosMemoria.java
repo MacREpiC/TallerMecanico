8d669aae727086cb3bdc52090eddc62a4d5c911b
MacREpiC <brosgenepic@gmail.com>
2024-03-14 11:06:30 +0100

    FabricaFuenteDatos, FabricaFuenteDatosMemoeria, IFuenteDatos terminados

--- src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/negocio/memoria/FuenteDatosMemoria.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/negocio/memoria/FuenteDatosMemoria.java
@@ -1,20 +1,22 @@
 package org.iesalandalus.programacion.tallermecanico.modelo.negocio.memoria;
 
 import org.iesalandalus.programacion.tallermecanico.modelo.negocio.IClientes;
+import org.iesalandalus.programacion.tallermecanico.modelo.negocio.IFuenteDatos;
 import org.iesalandalus.programacion.tallermecanico.modelo.negocio.ITrabajos;
 import org.iesalandalus.programacion.tallermecanico.modelo.negocio.IVehiculos;
 
-public enum FuenteDatosMemoria implements org.iesalandalus.programacion.tallermecanico.modelo.negocio.IFuenteDatos {
-    MEMORIA;
-
+public class FuenteDatosMemoria implements IFuenteDatos {
+    @Override
     public IClientes crearClientes() {
         return new Clientes();
     }
 
+    @Override
     public IVehiculos crearVehiculos() {
         return new Vehiculos();
     }
 
+    @Override
     public ITrabajos crearTrabajos() {
         return new Trabajos();
     }
